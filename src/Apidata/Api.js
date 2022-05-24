c12ab75ee3332f40c33ea44a421f4a7a976f8c84
Ivory <ivory@Apples-MacBook-Pro.local>
2022-05-24 12:11:33 +0530

    first commit

---
+++ src/Apidata/Api.js
@@ -0,0 +1,37 @@
+export const apidata = [
+  {
+    id: 1,
+    name: "sanjay",
+    city: "indore",
+  },
+  {
+    id: 2,
+    name: "jhon",
+    city: "ahemdabad",
+  },
+  {
+    id: 3,
+    name: "joy",
+    city: "dewas",
+  },
+  {
+    id: 4,
+    name: "rahul",
+    city: "ratlam",
+  },
+  {
+    id: 5,
+    name: "aj",
+    city: "ujjain",
+  },
+  {
+    id: 6,
+    name: "deepak",
+    city: "indore",
+  },
+  {
+    id: 7,
+    name: "Lokendra",
+    city: "bhopal",
+  },
+];
--- src/App.js
+++ src/App.js
@@ -1,57 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import {
+  addData,
+  deleteName,
+  updateUser,
+} from "./features/counter/counterSlice";
 
 function App() {
+  const apidata = useSelector((state) => state.data.value);
+  const dispatch = useDispatch();
+  const [addname, setaddName] = useState("");
+  const [addcity, setaddCity] = useState("");
+  const [updatenewname, setupdatenewName] = useState("");
+
+  // console.log(updatenewname);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <br />
+      <br />
+      <input type="text" onChange={(e) => setaddName(e.target.value)} />
+      <input type="text" onChange={(e) => setaddCity(e.target.value)} />
+      <button
+        onClick={() =>
+          dispatch(
+            addData({
+              id: apidata[apidata.length - 1].id + 1,
+              name: addname,
+              city: addcity,
+            })
+          )
+        }
+        className="btn-btn"
+      >
+        Save data
+      </button>
+      <br />
+      {apidata.map((el, i) => {
+        return (
+          <div className="center" key={i}>
+            <div className="App">
+              <h1>id : -{el.id}</h1>
+              <h1>Name :-{el.name}</h1>
+              <h1>City :-{el.city}</h1>
+              <input
+                type="text"
+                placeholder="Name..."
+                onChange={(e) => setupdatenewName(e.target.value)}
+              />
+              <button
+                onClick={() =>
+                  dispatch(updateUser({ id: el.id, name: updatenewname }))
+                }
+                className="btn-btn"
+              >
+                Update Name
+              </button>
+              <button
+                onClick={() => dispatch(deleteName({ id: el.id }))}
+                className="btn-btn"
+              >
+                delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
